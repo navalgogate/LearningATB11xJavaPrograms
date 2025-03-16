520257d27b180b5daebe9dd21f1806fa742329a9
student <navalgogate@gmail.com>
2025-03-16 13:35:52 +0530

    Lab004 Added 10th March

---
+++ src/ex_01_JavaBasic/Lab004_MultipleMainMethod.java
@@ -0,0 +1,16 @@
+package ex_01_JavaBasic;
+
+public class Lab004_MultipleMainMethod {
+
+    public static void main(String[] args) {
+        System.out.println("Main1");
+    }
+
+//    public static void main(String[] args) {
+//        System.out.println("Main2");
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println("Main3");
+//    }
+}
