749786050be808a31d7fd8f0815a2fcc52dc71d8
student <navalgogate@gmail.com>
2025-03-17 20:54:20 +0530

    Lab005 Added 11th March

---
+++ src/ex_01_JavaBasic/Lab005_Variables.java
@@ -0,0 +1,10 @@
+package ex_01_JavaBasic;
+
+public class Lab005_Variables {
+    public static void main(String[] args) {
+        int a = 10;
+        a = 10+1;
+        a = 10;
+        System.out.println(a);
+    }
+}
