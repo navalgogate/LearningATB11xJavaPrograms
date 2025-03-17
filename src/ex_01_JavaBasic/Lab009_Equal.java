a2a323fbc912e8a55856c2fc55edbf071d14be1e
student <navalgogate@gmail.com>
2025-03-18 01:19:11 +0530

    Lab009 Added 12th March

---
+++ src/ex_01_JavaBasic/Lab009_Equal.java
@@ -0,0 +1,24 @@
+package ex_01_JavaBasic;
+
+public class Lab009_Equal {
+    public static void main(String[] args) {
+        System.out.println(10==10);
+        System.out.println(10<10);
+        System.out.println(10>=10);
+        System.out.println(!(10>20));
+
+        String name = "Naval";
+        String Last = "Gogate";
+
+        int a = 10;
+        int b = 20;
+
+
+        System.out.println(name + Last + a + b);
+        System.out.println(name + Last + (a + b));
+        System.out.println(a + b + name + Last);
+
+
+
+    }
+}
