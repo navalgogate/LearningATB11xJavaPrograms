7ae0e3b1a6c573f6d05bf5d0945c604bbe753d24
student <navalgogate@gmail.com>
2025-03-17 23:16:56 +0530

    Lab008 Added 11th March

---
+++ src/ex_01_JavaBasic/Lab008_Constants.java
@@ -0,0 +1,18 @@
+package ex_01_JavaBasic;
+
+public class Lab008_Constants {
+    public static void main(String[] args) {
+
+           int  a = 10;
+           byte b = 11;
+           short c = 12;
+           long d = 8007123423l;
+        final  float PI = 3.14f;
+            double f = 87.7765258944;
+            boolean g = true;
+            char h = 'N';
+
+
+        System.out.println(PI);
+    }
+}
